0ab1ba7bf2ccba442bace89e4c3f0011c333b9a1
Artem <slivnitsin.art@mail.ru>
2022-09-08 04:25:21 +0800

    task_43

---
+++ task_43/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+ 
+if((k1 * b2 - k2 * b1) == 0)
+{
+    Console.Write("Прямые параллельны, либо совпадают");
+}
+else
+{ 
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
+}
